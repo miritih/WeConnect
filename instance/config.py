de845b64bf1e3215fda2459738ab62e6fa596d86
Eric Mwenda <ericmwenda5@gmail.com>
2018-03-05 12:35:13 +0300

    testing

---
+++ instance/config.py
@@ -0,0 +1,30 @@
+import os
+
+
+class Config(object):
+    """docstring for Config"""
+    DEBUG = False
+    SECRET = os.getenv('SECRETS')
+
+
+class Development(Config):
+    """docstring for Development"""
+    DEBUG = True
+
+
+class Testing(Config):
+    """docstring for Testing"""
+    DEBUG = True
+    TESTING = True
+
+
+class Production(Config):
+    """"""
+    DEBUG = False
+    TESTING = False
+
+app_config = {
+    'development': Development,
+    'testing': Testing,
+    'production': Production,
+}

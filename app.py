1eaf405dfd7d1b09f44783a0cdc66a91209ecbeb
Eric Mwenda <ericmwenda5@gmail.com>
2018-03-06 22:49:52 +0300

    fix confif missing error

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ bp = Blueprint('app', __name__)
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.register_blueprint(bp)
     return app

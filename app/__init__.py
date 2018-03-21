c449328f664ae35fd972540bc9ab0cf3dea9813b
mwenda-eric <ericmwenda5@gmail.com>
2018-03-21 12:21:28 +0000

    [Finishes #155544643] set app to use postgresql database

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,26 @@
 from flask import Flask, Blueprint
 import os
 from instance.config import app_config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from app.v1 import version1
 
+db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.register_blueprint(version1, url_prefix="/api/v1/")
+    # initialize extensions
+    migrate = Migrate(app, db)
+    db.init_app(app)
     return app
+    
+import app.models.v2
 
-config_name = os.getenv('APP_SETTINGS')
-app = create_app(config_name)
+
+
+
+
+

8bff04aaeae57373eb3f21cb8f3b55111c1a872b
mwenda-eric <ericmwenda5@gmail.com>
2018-03-21 12:22:45 +0000

    [Finishes #155548672] set app to use SQLAchemy

--- instance/config.py
+++ instance/config.py
@@ -1,21 +1,19 @@
 import os
-
-
 class Config(object):
     """docstring for Config"""
     DEBUG = False
     TESTING = False
-
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    
 class Development(Config):
     """docstring for Development"""
     DEBUG = True
-
 
 class Testing(Config):
     """docstring for Testing"""
     DEBUG = True
     TESTING = True
-
+    SQLALCHEMY_DATABASE_URI = "postgres:///weconnect_test"
 
 class Production(Config):
     """"""
--- run.py
+++ run.py
@@ -1,4 +1,8 @@
-from app import app
+from app import create_app
+import os
+
+config_name = os.getenv('APP_SETTINGS')
+app = create_app(config_name)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8080)

89eb0b3400ba03bb9c45e3256b7b0af832303072
mwenda-eric <ericmwenda5@gmail.com>
2018-03-22 08:58:04 +0000

    [feature ] Add timestamps to db models

--- app/models/v2.py
+++ app/models/v2.py
@@ -1,7 +1,21 @@
 from app import db
 
-
-class User(db.Model):
+class TimestampMixin(object):
+    """
+    This will add created_at and 
+    updated_at timestamps to every table
+    """
+    created_at = db.Column(
+        db.DateTime, 
+        server_default=db.func.now()
+        )
+    updated_at = db.Column(
+        db.DateTime,
+        server_default=db.func.now(),
+        server_onupdate=db.func.now()
+        )
+    
+class User(TimestampMixin, db.Model):
     """model to create table users for storing user data"""
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -10,12 +24,18 @@ class User(db.Model):
     first_name = db.Column(db.String(255))
     last_name = db.Column(db.String(255))
     businesses = db.relationship(
-        'Business', backref='bsowner', cascade='all, delete-orphan')
+        'Business',
+        backref='bsowner',
+        cascade='all, delete-orphan'
+        )
     reviews = db.relationship(
-        'Review', backref='rvwowner', cascade='all, delete-orphan')
+        'Review',
+        backref='rvwowner',
+        cascade='all, delete-orphan'
+        )
 
 
-class Business(db.Model):
+class Business(TimestampMixin, db.Model):
     """ Creates business table"""
     __tablename__ = 'businesses'
     id = db.Column(db.Integer, primary_key=True)
@@ -25,14 +45,23 @@ class Business(db.Model):
     bio = db.Column(db.String(255))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     reviews = db.relationship(
-        'Review', backref='rvwbusines', cascade='all, delete-orphan')
+        'Review', 
+        backref='rvwbusines', 
+        cascade='all, delete-orphan'
+        )
 
 
-class Review(db.Model):
+class Review(TimestampMixin, db.Model):
     """ Creates Reviews table"""
     __tablename__ = 'reviews'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255))
     body = db.Column(db.String(255))
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    business_id = db.Column(db.Integer, db.ForeignKey('businesses.id'))
+    user_id = db.Column(
+        db.Integer, 
+        db.ForeignKey('users.id')
+        )
+    business_id = db.Column(
+        db.Integer, 
+        db.ForeignKey('businesses.id')
+        )

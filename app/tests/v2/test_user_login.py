1e51be2fd63a8e5252f1a13e70afce17e5967188
Eric Mwenda <ericmwenda5@gmail.com>
2018-03-25 14:55:34 +0300

    Add user login tests

---
+++ app/tests/v2/test_user_login.py
@@ -0,0 +1,74 @@
+import unittest
+import os
+import json
+from app import create_app
+from app.models.v2 import User
+
+
+class LoginUserTestCase(unittest.TestCase):
+    """This class implements Login user TestCases"""
+
+    def setUp(self):
+        """Will be called before every test"""
+        config_name = os.getenv('APP_SETTINGS')
+        self.app = create_app(config_name)
+        self.client = self.app.test_client
+        self.user = {
+            "username": "mwenda",
+            "email": "ericmwenda5@gmail.com",
+            "password": "qwerty123!@#",
+            "first_name": "eric",
+            "last_name": "Miriti"
+        }
+        self.logins = {
+            "username": "mwenda",
+            "password": "qwerty123!@#"
+        }
+        # Create_user
+        self.client().post(
+            '/api/v2/auth/register',
+            data=json.dumps(self.user),
+            headers={"content-type": "application/json"}
+        )
+
+    def tearDown(self):
+        """ clear data after every test"""
+        User.query.delete()
+
+    def test_user_can_login(self):
+        """Test user can login to get access token"""
+        login = self.client().post(
+            '/api/v2/auth/login',
+            data=json.dumps(self.logins),
+            headers={"content-type": "application/json"}
+        )
+        self.assertEqual(login.status_code, 200)
+        self.assertIn("auth_token", str(login.data))
+
+    def test_cannot_login_if_not_registered(self):
+        """ Test that only registered users can login"""
+        User.query.delete()  # clears users
+        login = self.client().post(
+            '/api/v2/auth/login',
+            data=json.dumps(self.logins),
+            headers={"content-type": "application/json"}
+        )
+        self.assertEqual(login.status_code, 401)
+        self.assertIn("Username not found!", str(login.data))
+
+    def test_login_details_required(self):
+        """Test that all login fields are required"""
+        login = self.client().post(
+            '/api/v2/auth/login',
+            data=json.dumps({
+                "username": "",
+                "password": ""
+            }),
+            headers={"content-type": "application/json"}
+        )
+        self.assertEqual(login.status_code, 401)
+        self.assertIn("Field cannot be empty", str(login.data))
+
+
+if __name__ == "__main__":
+    unittest.main()

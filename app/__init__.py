1b0bc4d6abba82718912cbf544c1304a7fa500d9
mwenda-eric <ericmwenda5@gmail.com>
2018-04-11 02:10:29 +0300

    Add CORS to allow cross-origin AJAX requests

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,9 @@ import os
 from instance.config import app_config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS, cross_origin
 from app.v1 import version1
+
 
 db = SQLAlchemy()
 
@@ -11,6 +13,7 @@ db = SQLAlchemy()
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
+    CORS(app) #makes cross-origin AJAX possible.
     app.config.from_pyfile('config.py')
     app.register_blueprint(version1, url_prefix="/api/v1/")
     from app.v2 import version2
